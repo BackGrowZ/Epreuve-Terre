afb3631d84e3ceee3c5a4cc597b3cd029d16440d
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2022-11-05 16:10:18 +0100

    Update terre02.js

--- terre02.js
+++ terre02.js
@@ -3,25 +3,10 @@
 // recuperer la liste des argument au quel on retire le "node" et le nom du fichier
 const ARG = process.argv.slice(2);
 
-// Resultat en vert
-const returnResult = (text) => {
-  const COLOR_GREEN = "\x1b[32m";
-  const COLOR_RESET = "\x1b[0m";
-  console.log(COLOR_GREEN, text, COLOR_RESET);
-};
-
-// Error de couleur rouge
-const returnError = (text = null) => {
-  const COLOR_RED = "\x1b[31m";
-  const COLOR_RESET = "\x1b[0m";
-  const msg = typeof text === "string" ? text : "Error";
-  console.error(COLOR_RED, msg, COLOR_RESET);
-};
-
 // donne la suite de l'aphabet
 const showArg = (args) => {
   if (haveArg(2)) {
-    args.forEach((arg) => returnResult(arg));
+    args.forEach((arg) => console.log(arg));
   }
 };
 
@@ -30,7 +15,7 @@ const haveArg = (length = 1) => {
   if (ARG.length >= length) {
     return true;
   }
-  returnError("Il vous faut au moins 2 arguments");
+  console.error("Il vous faut au moins 2 arguments");
   return false;
 };
 
